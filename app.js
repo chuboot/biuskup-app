19c8eb5da0b2f8e919016b249ae38cf39e5886d9
chuboot <chuboot.id@gmail.com>
2022-10-23 15:06:41 +0800

    add swiper js for carosel

--- app.js
+++ app.js
@@ -1,3 +1,76 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const slidesCom = document.querySelector(".swiper-slide");
+
+fetch(
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=17c8dffc0cbd61894a0460817bbba88e&language=en-US&page=1"
+)
+  .then((response) => response.json())
+  .then(
+    (response) => {
+      const moviess = response.results;
+      const movies = moviess.slice(0, 5);
+      // let cards = "";
+      // console.log(movies.slice(0, 3));
+      let test = "";
+      renderAllSlides(movies);
+      // for (const movie of movies) {
+      // test = `${movie.title},test,https://www.themoviedb.org/t/p/original/${movie.poster_path},https://codepen.io/emanuelemicheletti/pen/abYarrv,View this movie`;
+      //   myArray.push(
+      //     movie.title,
+      //     "test",
+      //     "https://www.themoviedb.org/t/p/original/ " + movie.poster_path + "",
+      //     "https://codepen.io/emanuelemicheletti/pen/abYarrv",
+      //     "View Movie"
+      //   );
+
+      //   console.log(
+      //     "https://www.themoviedb.org/t/p/original/" + movie.poster_path
+      //   );
+      console.log(slidesCom);
+    }
+    //   }
+  );
+
+const renderAllSlides = (slides) => {
+  const slidesCom = document.querySelector(".swiper-wrapper");
+
+  slidesCom.innerHTML = "";
+
+  slides.forEach((sld) => {
+    slidesCom.innerHTML += `
+        
+        <div class="swiper-slide">
+              <img
+                src="https://www.themoviedb.org/t/p/original/${sld.backdrop_path}"
+                alt=""
+              />
+              <div class="caption">
+                <span>${sld.release_date}</span>
+                <h1>${sld.title}</h1>
+                <button class="btn">View Movie</button>
+                <button class="btn-p">Watch Trailer</button>
+              </div>
+            </div>
+      `;
+  });
+};
+
 //change bg when scroll
 let navbar = document.querySelector("header");
 
